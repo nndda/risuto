b0606a95a2cbfb52771c1b1343e8399369b34be1
nnda <96333146+nndda@users.noreply.github.com>
2024-02-04 01:51:56 +0800

    Separate entries

--- index.ts
+++ index.ts
@@ -1,7 +1,2 @@
-import "@fontsource/noto-sans-jp";
-import "@fortawesome/fontawesome-free/css/fontawesome.min.css";
-import "@fortawesome/fontawesome-free/css/solid.min.css";
-import "@fortawesome/fontawesome-free/css/brands.min.css";
-
-import "./src/app.ts";
-import "./src/style.css";
+import "./src/app/main";
+import "./src/style/style.css";
