52411531ef7ca71efd35052ec426d8badb400456
nnda <96333146+nndda@users.noreply.github.com>
2024-02-04 16:33:18 +0800

    Implement object for user data

--- src/app/storage.ts
+++ src/app/storage.ts
@@ -1,13 +1,55 @@
-function localStorageHas(item : string) {
+export function storageHas(item : string) {
   return localStorage.getItem(item) != null
 }
 
-function saveData() {
-  
+export interface userData {
+  listCollections : Array<List>,
+  darkMode : boolean,
 }
 
-function initStorage() {
-  if (localStorageHas("saved-data")) {
+export interface List {
+  title : String,
+  currency : String,
+  displayUnchecked : boolean,
+  items : Array<ListItem>,
+}
+
+export interface ListItem {
+  name : String,
+  qty : number,
+  price : number,
+}
+
+export function rows2ListData(rows : HTMLCollectionOf<Element>) {
+  let list : List = {
+    title : (<HTMLInputElement>document.getElementById("list-title")).value,
+    currency : (<HTMLInputElement>document.getElementById("list-currency")).value,
+    displayUnchecked : (<HTMLInputElement>document.getElementById("show-not-done")).checked,
+    items : [],
+  };
+
+  for (let i = rows.length - 1; i >= 0; i--) {
+    let item : ListItem = {
+      name : (<HTMLInputElement>rows[i].getElementsByClassName("item-name")[0]).value,
+
+      qty : parseInt(
+        (<HTMLInputElement>rows[i].getElementsByClassName("item-qty")[0])
+        .value, 10
+      ),
+
+      price : parseFloat(
+        (<HTMLInputElement>rows[i].getElementsByClassName("item-price")[0])
+        .value
+      ),
+    };
+
+    list.items.push(item)
+  }
+  return list
+}
+
+export function storageInit() {
+  if (storageHas("saved-data")) {
 
   }
 }
