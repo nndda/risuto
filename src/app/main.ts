6b412056de94478781c1111ff7e9e22a128ad39d
nnda <96333146+nndda@users.noreply.github.com>
2024-02-04 16:32:29 +0800

    Refactor calculations

--- src/app/main.ts
+++ src/app/main.ts
@@ -3,12 +3,22 @@ const d = document;
 import "./storage";
 
 import { Currency, getCurrency, initCurrencySelector } from "./currency";
+import { rows2ListData } from "./storage";
 let currentCurrency : string = "IDR";
 let curr : Currency = getCurrency();
 
 const currChangeBtn = <HTMLSelectElement>d.getElementById("list-currency");
 currChangeBtn.onchange = () => {
   currentCurrency = currChangeBtn.value;
+
+  const itemTotals = table.getElementsByClassName("item-total-display");
+  for (let i = itemTotals.length - 1; i >= 0; i--) {
+    itemTotals[i].textContent = curr[currentCurrency].format(
+      parseFloat(
+        itemTotals[i].getAttribute("data-total-num")
+      )
+    );
+  }
   calcTotal();
 };
 initCurrencySelector(currChangeBtn);
@@ -31,9 +41,21 @@ function addItemRow() {
     ).oninput = () => {
     checkErr(new_row);
   };
+  const buttonDelete = (<HTMLButtonElement>new_row.getElementsByClassName("button-del")[0]);
+  buttonDelete.onclick = removeRow;
+
+  buttonDelete.classList.toggle("hidden",
+    table.getElementsByClassName("row").length < 1
+  )
 
   table.appendChild(new_row);
   checkErr(new_row);
+}
+
+// prob. not a gud idea
+function removeRow() {
+  this.parentElement.parentElement.remove();
+  calcTotal();
 }
 
 addItem.onclick = () => {
@@ -49,7 +71,7 @@ function checkErr(row : HTMLElement) {
   let min = 0;
   let error = false;
 
-  for (var i = data.length - 1; i >= 0; i--) {
+  for (let i = data.length - 1; i >= 0; i--) {
     let value = parseFloat(data[i].value);
     if (value > 0) {
       if (value <= 0) min += 1;
@@ -58,36 +80,49 @@ function checkErr(row : HTMLElement) {
   }
 
   error = (valid < 2) || (min > 0);
-  // console.log(error);
 
   row.classList.toggle("row-error", error);
-  row.getElementsByClassName("item-total")[0].classList.toggle("row-valid", !error);
+  row.classList.toggle("row-valid", !error);
   row.getElementsByClassName("check-done")[0].toggleAttribute("disabled", error);
-  if (!error) {
-    calc(row);
-  }
+
+  calc(row, error);
 }
 
-function calc(row : HTMLElement) {
-  let qty = parseInt(
-    (<HTMLInputElement>row.getElementsByClassName("item-qty")[0]).value, 10);
-  let price = parseFloat(
-    (<HTMLInputElement>row.getElementsByClassName("item-price")[0]).value);
-  let totalElem = <HTMLInputElement>row.getElementsByClassName("row-valid")[0];
-  totalElem.textContent = `${qty * price}`;
+function calc(row : HTMLElement, isError : boolean = false) {
+  let totalElem = <HTMLInputElement>row.getElementsByClassName("item-total-display")[0];
+
+  if (isError) {
+    totalElem.setAttribute("data-total-num", "0")
+  }
+  else {
+    let qty = parseInt(
+      (<HTMLInputElement>row.getElementsByClassName("item-qty")[0]).value, 10);
+    let price = parseFloat(
+      (<HTMLInputElement>row.getElementsByClassName("item-price")[0]).value);
+    let total = qty * price;
+
+    totalElem.setAttribute("data-total-num", `${total}`)
+  }
+
+  totalElem.textContent = curr[currentCurrency].format(
+    parseFloat(
+      totalElem.getAttribute("data-total-num")
+    )
+  );
+
   calcTotal();
 }
 
 function calcTotal() {
   let totals = table.getElementsByClassName("row-valid");
-  let totalsDisplay = table.getElementsByClassName("item-total-display");
   let totalOutput = 0;
-  for (var i = totals.length - 1; i >= 0; i--) {
-    let num = parseFloat(totals[i].textContent);
-    totalOutput += num;
-    totalsDisplay[i].textContent = curr[currentCurrency].format(num);
+  for (let i = totals.length - 1; i >= 0; i--) {
+    totalOutput += parseFloat(totals[i]
+      .getElementsByClassName("item-total-display")[0]
+      .getAttribute("data-total-num"));
   }
   total.textContent = curr[currentCurrency].format(totalOutput);
+  console.log(rows2ListData(table.getElementsByClassName("row")));
 }
 
 // Initialization
