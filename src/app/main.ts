5af3907555f0c1f05a464cafa32bc8b456a4012b
nnda <96333146+nndda@users.noreply.github.com>
2024-02-04 22:28:58 +0800

    Separate checked total price

--- src/app/main.ts
+++ src/app/main.ts
@@ -2,13 +2,20 @@ const d = document;
 
 import "./storage";
 
+import { initColors } from "./theme";
+initColors();
+
 import { Currency, getCurrency, initCurrencySelector } from "./currency";
-import { rows2ListData } from "./storage";
+import { ListItem, rows2ListData, listData2Rows, sampleGrocery } from "./storage";
+
 let currentCurrency : string = "IDR";
 let curr : Currency = getCurrency();
 
 const currChangeBtn = <HTMLSelectElement>d.getElementById("list-currency");
 currChangeBtn.onchange = () => {
+  currencyChange();
+};
+export function currencyChange() {
   currentCurrency = currChangeBtn.value;
 
   const itemTotals = table.getElementsByClassName("item-total-display");
@@ -20,29 +27,65 @@ currChangeBtn.onchange = () => {
     );
   }
   calcTotal();
-};
+}
 initCurrencySelector(currChangeBtn);
 
 const addItem = <HTMLButtonElement>d.getElementById("add-item");
 const rowTemplate = d.getElementById("row-template");
 const table = d.getElementById("table");
 const total = d.getElementById("total");
+const totalChecked = d.getElementById("total-checked");
+const toggleShowUncheckedButton = <HTMLInputElement>d.getElementById("show-unchecked");
 
-function addItemRow() {
+function toggleShowUnchecked() {
+  const rows = table.getElementsByClassName("row");
+  for (let i = rows.length - 1; i >= 0; i--) {
+    if (toggleShowUncheckedButton.checked) {
+      rows[i].classList.remove("hidden");
+    }
+    else {
+      rows[i].classList.toggle("hidden",
+        (
+          <HTMLInputElement>rows[i]
+          .getElementsByClassName("check-done")[0])
+          .checked
+        );
+    }
+  }
+}
+toggleShowUncheckedButton.oninput = () => {
+  toggleShowUnchecked();
+};
+
+export function addItemRow(item : ListItem = {
+  name : "",
+  qty : 0,
+  price : 0,
+  checked : false,
+}) {
   let new_row = <HTMLElement>rowTemplate.cloneNode(true);
   new_row.removeAttribute("id");
   new_row.classList.remove("hidden");
+  new_row.setAttribute("data-checked", `${item.checked}`);
 
-  (<HTMLInputElement>new_row.getElementsByClassName("item-price")[0]
-    ).oninput = () => {
-    checkErr(new_row);
+  const checkButton = <HTMLInputElement>new_row.getElementsByClassName("check-done")[0];
+  checkButton.oninput = () => {
+    checkRow(new_row);
   };
-  (<HTMLInputElement>new_row.getElementsByClassName("item-qty")[0]
-    ).oninput = () => {
-    checkErr(new_row);
-  };
+
+  const itemPrice = <HTMLInputElement>new_row.getElementsByClassName("item-price")[0];
+  const itemName = <HTMLInputElement>new_row.getElementsByClassName("item-name")[0];
+  const itemQty = <HTMLInputElement>new_row.getElementsByClassName("item-qty")[0];
+
+  itemPrice.value = `${item.price}`;
+  itemName.value = `${item.name}`;
+  itemQty.value = `${item.qty}`;
+
+  itemPrice.oninput = () => {checkErr(new_row);};
+  itemQty.oninput = () => {checkErr(new_row);};
+
   const buttonDelete = (<HTMLButtonElement>new_row.getElementsByClassName("button-del")[0]);
-  buttonDelete.onclick = removeRow;
+  buttonDelete.onclick = () => {removeRow(new_row);};
 
   buttonDelete.classList.toggle("hidden",
     table.getElementsByClassName("row").length < 1
@@ -53,8 +96,20 @@ function addItemRow() {
 }
 
 // prob. not a gud idea
-function removeRow() {
-  this.parentElement.parentElement.remove();
+function removeRow(row : HTMLElement) {
+  row.remove();
+  calcTotal();
+}
+function checkRow(row : HTMLElement) {
+  row.setAttribute(
+    "data-checked",
+    `${(
+      <HTMLInputElement>row.getElementsByClassName("check-done")[0]
+    ).checked}`
+  )
+  if (toggleShowUncheckedButton.checked != true) {
+    row.classList.add("hidden");
+  }
   calcTotal();
 }
 
@@ -116,14 +171,24 @@ function calc(row : HTMLElement, isError : boolean = false) {
 function calcTotal() {
   let totals = table.getElementsByClassName("row-valid");
   let totalOutput = 0;
+  let totalOutputChecked = 0;
   for (let i = totals.length - 1; i >= 0; i--) {
-    totalOutput += parseFloat(totals[i]
+    let totalNum = parseFloat(totals[i]
       .getElementsByClassName("item-total-display")[0]
       .getAttribute("data-total-num"));
+
+    if (totals[i].getAttribute("data-checked") === "true") {
+      totalOutputChecked += totalNum
+    }
+
+    totalOutput += totalNum;
   }
   total.textContent = curr[currentCurrency].format(totalOutput);
-  console.log(rows2ListData(table.getElementsByClassName("row")));
+  totalChecked.textContent = curr[currentCurrency].format(totalOutputChecked);
+  // console.log(rows2ListData(table.getElementsByClassName("row")));
 }
 
 // Initialization
 addItemRow();
+
+listData2Rows(sampleGrocery);
