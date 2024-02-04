3274e0631ba38be4f9a2d7540e9d064e7e3bb023
nnda <96333146+nndda@users.noreply.github.com>
2024-02-04 22:25:01 +0800

    Add sample
    - Implement `listData2Rows`

--- src/app/storage.ts
+++ src/app/storage.ts
@@ -1,3 +1,5 @@
+import { addItemRow, currencyChange } from "./main";
+
 export function storageHas(item : string) {
   return localStorage.getItem(item) != null
 }
@@ -5,20 +7,54 @@ export function storageHas(item : string) {
 export interface userData {
   listCollections : Array<List>,
   darkMode : boolean,
+  accentColor : string,
 }
 
 export interface List {
-  title : String,
-  currency : String,
+  title : string,
+  currency : string,
   displayUnchecked : boolean,
   items : Array<ListItem>,
 }
 
 export interface ListItem {
-  name : String,
+  name : string,
   qty : number,
   price : number,
+  checked : boolean,
 }
+
+export const sampleGrocery : List = {
+  title : "This week's grocery",
+  currency : "USD",
+  displayUnchecked : true,
+  items : [
+    {
+      name : "Egg",
+      qty : 6,
+      price : 1.5,
+      checked : true,
+    },
+    {
+      name : "Milk",
+      qty : 2,
+      price : 12.15,
+      checked : true,
+    },
+    {
+      name : "Bread",
+      qty : 3,
+      price : 8.25,
+      checked : true,
+    },
+    {
+      name : "Coffee",
+      qty : 2,
+      price : 5.5,
+      checked : false,
+    },
+  ],
+};
 
 export function rows2ListData(rows : HTMLCollectionOf<Element>) {
   let list : List = {
@@ -41,11 +77,27 @@ export function rows2ListData(rows : HTMLCollectionOf<Element>) {
         (<HTMLInputElement>rows[i].getElementsByClassName("item-price")[0])
         .value
       ),
+
+      checked : (<HTMLInputElement>rows[i].getElementsByClassName("check-done")[0]).checked
     };
 
     list.items.push(item)
   }
   return list
+}
+
+export function listData2Rows(data : List) {
+  const currentRows = document.getElementById("table").getElementsByClassName("row");
+  for (let i = currentRows.length - 1; i >= 0; i--) {
+    currentRows[i].remove();
+  }
+  (<HTMLInputElement>document.getElementById("list-title")).value = data.title;
+  (<HTMLInputElement>document.getElementById("show-unchecked")).checked = data.displayUnchecked;
+  (<HTMLInputElement>document.getElementById("list-currency")).value = data.currency;
+  data.items.forEach((item) => {
+    addItemRow(item);
+  });
+  currencyChange();
 }
 
 export function storageInit() {
