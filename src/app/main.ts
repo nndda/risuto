fc1db6b11eae04f3f05fdc3487b71d52835a9156
nnda <96333146+nndda@users.noreply.github.com>
2024-02-09 23:02:54 +0800

    Implement storage

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,43 +1,115 @@
 const d = document;
 
-import "./storage";
-
 import { initColors } from "./theme";
 initColors();
 
-import { Currency, getCurrency, initCurrencySelector } from "./currency";
-import { ListItem, rows2ListData, listData2Rows, sampleGrocery } from "./storage";
+import { curr, currentCurrency, currChangeBtn } from "./currency";
+import { List, ListItem, row2ListItem, listData2Rows, storageSave, homeListInit } from "./storage";
 
-let currentCurrency : string = "IDR";
-let curr : Currency = getCurrency();
+const home = d.getElementById("home");
+export const savedListContainer = d.getElementById("saved-list");
 
-const currChangeBtn = <HTMLSelectElement>d.getElementById("list-currency");
-currChangeBtn.onchange = () => {
-  currencyChange();
-};
-export function currencyChange() {
-  currentCurrency = currChangeBtn.value;
+export const table = d.getElementById("table");
+const listContainer = d.getElementById("list-container");
 
-  const itemTotals = table.getElementsByClassName("item-total-display");
-  for (let i = itemTotals.length - 1; i >= 0; i--) {
-    itemTotals[i].textContent = curr[currentCurrency].format(
-      parseFloat(
-        itemTotals[i].getAttribute("data-total-num")
-      )
-    );
-  }
-  calcTotal();
+const newListBtn = <HTMLButtonElement>d.getElementById("new-list-btn");
+newListBtn.onclick = () => {
+  listNew();
 }
-initCurrencySelector(currChangeBtn);
+
+const backToHomeBtn = <HTMLButtonElement>d.getElementById("back-home-btn");
+backToHomeBtn.onclick = () => {
+  storageSave();
+  cleanRows();
+  toggleApp(false);
+}
+
+const deleteCurrentListBtn = <HTMLButtonElement>d.getElementById("delete-current-list-btn");
+deleteCurrentListBtn.onclick = () => {
+  localStorage.removeItem(currentList.title)
+  currentList = null;
+  homeListInit();
+  toggleApp(false);
+}
+
+function toggleApp(toApp = true) {
+  home.classList.toggle("toggle-app-display", toApp);
+  listContainer.classList.toggle("toggle-app-display", !toApp);
+}
+
+toggleApp(false);
+
+function listNew() {
+  cleanRows();
+  toggleApp(true);
+  let untitledTitleCounts = 0;
+
+  for (let n = localStorage.length - 1; n >= 0; n--) {
+    if (localStorage.key(n).startsWith("Untitled list")) {
+      untitledTitleCounts += 1;
+    }
+  }
+
+  currentList = {
+    title : `Untitled list ${untitledTitleCounts}`,
+    currency : "IDR",
+    displayUnchecked : false,
+    items : [],
+  };
+
+  listTitleInput.value = currentList.title;
+  currChangeBtn.value = currentList.currency;
+  toggleShowUncheckedButton.checked = currentList.displayUnchecked;
+  addItemRow();
+}
+
+export function listSwitch(list : List) {
+  cleanRows();
+  toggleApp(true);
+  currentList = list;
+  listData2Rows(currentList);
+}
+
+export function cleanRows() {
+  table.innerHTML = "";
+}
+
+export let currentList : List;
+export function currentListSet(list : List) {
+  currentList = list;
+}
 
 const addItem = <HTMLButtonElement>d.getElementById("add-item");
 const rowTemplate = d.getElementById("row-template");
-const table = d.getElementById("table");
 const total = d.getElementById("total");
 const totalChecked = d.getElementById("total-checked");
 const toggleShowUncheckedButton = <HTMLInputElement>d.getElementById("show-unchecked");
 
 const hiddenRowClassName = "hidden-collapse-height";
+
+const listTitleInput = <HTMLInputElement>d.getElementById("list-title");
+listTitleInput.oninput = () => {
+  // could be better
+  if (listTitleInput.value != "_CONFIG") {
+    localStorage.removeItem(currentList.title);
+    currentList.title = listTitleInput.value;
+    storageSave();
+  }
+};
+
+// let listTitle : string;
+// let disallowedListTitles : Array<string> = [];
+// function getStoragelistsTitles() {
+//   for (let n = localStorage.length - 1; n >= 0; n--) {
+//     let str = localStorage.key(n);
+//     if (str != "_CONFIG" && str != listTitle) {
+//       disallowedListTitles.push(str);
+//     }
+//   }
+// }
+// function validateListTitle() {
+
+// }
 
 const itemsTotalChecked = d.getElementById("items-checked");
 const itemsTotalUnchecked = d.getElementById("items-unchecked");
@@ -58,6 +130,7 @@ function toggleShowUnchecked() {
     }
   }
 }
+
 toggleShowUncheckedButton.oninput = () => {
   toggleShowUnchecked();
 };
@@ -67,44 +140,67 @@ export function addItemRow(item : ListItem = {
   qty : 0,
   price : 0,
   checked : false,
-}) {
+}, fromStorage = false) {
   let new_row = <HTMLElement>rowTemplate.cloneNode(true);
   new_row.removeAttribute("id");
   new_row.classList.remove(hiddenRowClassName);
+  new_row.classList.remove("hidden");
   new_row.setAttribute("data-checked", `${item.checked}`);
 
   const checkButton = <HTMLInputElement>new_row.getElementsByClassName("check-done")[0];
-  const itemPrice = <HTMLInputElement>new_row.getElementsByClassName("item-price")[0];
   const itemName = <HTMLInputElement>new_row.getElementsByClassName("item-name")[0];
+  const itemPrice = <HTMLInputElement>new_row.getElementsByClassName("item-price")[0];
   const itemQty = <HTMLInputElement>new_row.getElementsByClassName("item-qty")[0];
 
   checkButton.checked = item.checked;
-  itemPrice.value = `${item.price}`;
   itemName.value = `${item.name}`;
+  itemPrice.value = `${item.price}`;
   itemQty.value = `${item.qty}`;
 
-  checkButton.oninput = () => {checkRow(new_row);};
-  itemPrice.oninput = () => {checkErr(new_row);};
-  itemQty.oninput = () => {checkErr(new_row);};
+  checkButton.oninput = () => {
+    checkRow(new_row);
+    updateRow(new_row);
+  };
+  itemName.oninput = () => {
+    updateRow(new_row);
+    storageSave();
+  };
+  itemPrice.oninput = () => {
+    checkErr(new_row);
+    updateRow(new_row);
+  };
+  itemQty.oninput = () => {
+    checkErr(new_row);
+    updateRow(new_row);
+  };
 
   const buttonDelete = (<HTMLButtonElement>new_row.getElementsByClassName("button-del")[0]);
-  buttonDelete.onclick = () => {removeRow(new_row);};
+  buttonDelete.onclick = () => {
+    removeRow(new_row);
+  };
 
   new_row.setAttribute("data-checked", `${item.checked}`);
 
-  buttonDelete.classList.toggle(hiddenRowClassName,
-    table.getElementsByClassName("row").length < 1
-  )
-
   table.appendChild(new_row);
   checkErr(new_row);
+
+  if (!fromStorage) {
+    currentList.items.push(row2ListItem(new_row));
+  }
 }
 
-// prob. not a gud idea
+function updateRow(row : HTMLElement) {
+  let idx : number = Array.prototype.indexOf.call(row.parentElement.children, row);
+  currentList.items[idx] = row2ListItem(row);
+}
+
 function removeRow(row : HTMLElement) {
+  let idx : number = Array.prototype.indexOf.call(row.parentElement.children, row);
+  currentList.items.splice(idx, 1);
   row.remove();
   calcTotal();
 }
+
 function checkRow(row : HTMLElement) {
   row.setAttribute(
     "data-checked",
@@ -173,7 +269,7 @@ function calc(row : HTMLElement, isError : boolean = false) {
   calcTotal();
 }
 
-function calcTotal() {
+export function calcTotal() {
   let totals = table.getElementsByClassName("row-valid");
   let totalOutput = 0;
   let totalOutputChecked = 0;
@@ -191,12 +287,13 @@ function calcTotal() {
   total.textContent = curr[currentCurrency].format(totalOutput);
   totalChecked.textContent = curr[currentCurrency].format(totalOutputChecked);
 
-  itemsTotalUnchecked.textContent = `${table.getElementsByClassName("row").length}`
-  itemsTotalChecked.textContent = `${table.querySelectorAll(".row[data-checked=\"true\"]").length}`
-  // console.log(rows2ListData(table.getElementsByClassName("row")));
+  itemsTotalUnchecked.textContent = `${table.getElementsByClassName("row").length}`;
+  itemsTotalChecked.textContent = `${table.querySelectorAll(".row[data-checked=\"true\"]").length}`;
+  storageSave();
 }
 
-// Initialization
-addItemRow();
+localStorage.setItem("_CONFIG", JSON.stringify({}));
 
-listData2Rows(sampleGrocery);
+// d.getElementById("dbg-print").onclick = () => {
+//   console.log(currentList);
+// };
--- src/app/storage.ts
+++ src/app/storage.ts
@@ -1,4 +1,7 @@
-import { addItemRow, currencyChange } from "./main";
+import { addItemRow, currentList, cleanRows, listSwitch } from "./main";
+import { currencyChange } from "./currency";
+
+const savedListContainer = document.getElementById("saved-list");
 
 export function storageHas(item : string) {
   return localStorage.getItem(item) != null
@@ -24,68 +27,36 @@ export interface ListItem {
   checked : boolean,
 }
 
-export const sampleGrocery : List = {
-  title : "This week's grocery",
-  currency : "USD",
-  displayUnchecked : true,
-  items : [
-    {
-      name : "Egg",
-      qty : 6,
-      price : 1.5,
-      checked : true,
-    },
-    {
-      name : "Milk",
-      qty : 2,
-      price : 12.15,
-      checked : true,
-    },
-    {
-      name : "Bread",
-      qty : 3,
-      price : 8.25,
-      checked : false,
-    },
-    {
-      name : "Coffee",
-      qty : 2,
-      price : 5.5,
-      checked : true,
-    },
-    {
-      name : "Chips",
-      qty : 1,
-      price : 3.75,
-      checked : false,
-    },
-  ],
-};
+export const sampleGrocery : List = require("../config/sampleList.json");
 
-export function rows2ListData(rows : HTMLCollectionOf<Element>) {
-  let list : List = {
-    title : (<HTMLInputElement>document.getElementById("list-title")).value,
-    currency : (<HTMLInputElement>document.getElementById("list-currency")).value,
-    displayUnchecked : (<HTMLInputElement>document.getElementById("show-not-done")).checked,
-    items : [],
-  };
-
-  for (let i = rows.length - 1; i >= 0; i--) {
-    let item : ListItem = {
-      name : (<HTMLInputElement>rows[i].getElementsByClassName("item-name")[0]).value,
+export function row2ListItem(row : HTMLElement) : ListItem {
+  return {
+      name : (<HTMLInputElement>row.getElementsByClassName("item-name")[0]).value,
 
       qty : parseInt(
-        (<HTMLInputElement>rows[i].getElementsByClassName("item-qty")[0])
+        (<HTMLInputElement>row.getElementsByClassName("item-qty")[0])
         .value, 10
       ),
 
       price : parseFloat(
-        (<HTMLInputElement>rows[i].getElementsByClassName("item-price")[0])
+        (<HTMLInputElement>row.getElementsByClassName("item-price")[0])
         .value
       ),
 
-      checked : (<HTMLInputElement>rows[i].getElementsByClassName("check-done")[0]).checked
-    };
+      checked : (<HTMLInputElement>row.getElementsByClassName("check-done")[0]).checked
+  }
+}
+
+export function rows2List(rows : HTMLCollectionOf<Element>) : List {
+  let list : List = {
+    title : (<HTMLInputElement>document.getElementById("list-title")).value,
+    currency : (<HTMLInputElement>document.getElementById("list-currency")).value,
+    displayUnchecked : (<HTMLInputElement>document.getElementById("show-unchecked")).checked,
+    items : [],
+  };
+
+  for (let i = rows.length - 1; i >= 0; i--) {
+    let item : ListItem = row2ListItem(<HTMLElement>rows[i]);
 
     list.items.push(item)
   }
@@ -93,21 +64,58 @@ export function rows2ListData(rows : HTMLCollectionOf<Element>) {
 }
 
 export function listData2Rows(data : List) {
-  const currentRows = document.getElementById("table").getElementsByClassName("row");
-  for (let i = currentRows.length - 1; i >= 0; i--) {
-    currentRows[i].remove();
-  }
+  cleanRows();
   (<HTMLInputElement>document.getElementById("list-title")).value = data.title;
   (<HTMLInputElement>document.getElementById("show-unchecked")).checked = data.displayUnchecked;
   (<HTMLInputElement>document.getElementById("list-currency")).value = data.currency;
   data.items.forEach((item) => {
-    addItemRow(item);
+    addItemRow(item, true);
   });
   currencyChange();
 }
 
-export function storageInit() {
-  if (storageHas("saved-data")) {
-
+function storageInit() {
+  if (localStorage.length < 1) {
+    localStorage.setItem(
+      sampleGrocery.title,
+      JSON.stringify(sampleGrocery));
+    homeListInit();
+  } else {
+    storageLoad();
   }
 }
+
+export function storageSave() {
+  localStorage.setItem(
+    currentList.title,
+    JSON.stringify(currentList)
+  )
+}
+
+export function storageLoad() {
+  homeListInit();
+}
+
+export function homeListInit() {
+  savedListContainer.innerHTML = "";
+  for (let n = localStorage.length - 1; n >= 0; n--) {
+    if (localStorage.key(n) != "_CONFIG") {
+      let data : List = JSON.parse(localStorage.getItem(localStorage.key(n)));
+
+      let listBtn = document.createElement("button");
+      listBtn.classList.add("button-wide");
+
+      listBtn.setAttribute("data-list-name", data.title)
+      listBtn.textContent = data.title;
+
+      listBtn.onclick = () => {
+        listSwitch(data);
+        // currentListSet(data);
+      };
+
+      savedListContainer.append(listBtn);
+    }
+  };
+}
+
+storageInit();
